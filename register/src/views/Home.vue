37a3bc5262a50a0e65ec679cff16d143f7285337
narababa18 <narababa18@gmail.com>
2021-07-15 03:21:41 +0700

    2

---
+++ register/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+        <v-app-bar 
+        color="primary" 
+        dense 
+        flat 
+        dark
+        >
+            <v-toolbar-title>ลงทะเบียน</v-toolbar-title>
+        </v-app-bar>
+  </div>
+</template>
+
+<script>
+
+</script>
